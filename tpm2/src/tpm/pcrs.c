6cb8eb935bb7b61abb37841b524e8d4a66059a3b
b-carr <53411033+b-carr@users.noreply.github.com>
2020-10-22 09:51:18 -0700

    Moved PCR string parsing to its own function.

--- tpm2/src/tpm/pcrs.c
+++ tpm2/src/tpm/pcrs.c
@@ -53,68 +53,30 @@ int init_pcr_selection(TSS2_SYS_CONTEXT * sapi_ctx, char *pcrs_string,
     kmyth_log(LOG_DEBUG,
               "converting user supplied PCR selection string = \"%s\"",
               pcrs_string);
+    bool *pcrs_list = NULL;
 
-    char *pcrs_string_cur = pcrs_string;
-    char *pcrs_string_next = NULL;
-    long pcr_index_l;
-    int pcr_index;
-
-    while (*pcrs_string_cur != '\0')
+    pcrs_list = malloc(numPCRs * sizeof(bool));
+    if (pcrs_list == NULL)
     {
-      pcr_index_l = strtol(pcrs_string_cur, &pcrs_string_next, 10);
-
-      // Check for overflow or underflow on the strtol call. There
-      // really shouldn't be, because the number of PCRs is small.
-      if ((pcr_index_l == LONG_MIN) || (pcr_index_l == LONG_MAX))
-      {
-        kmyth_log(LOG_ERR, "invalid PCR value specified ... exiting");
-        return 1;
-      }
-
-      // Check that strtol didn't fail to parse an integer, which is the only
-      // condition that would cause the pointers to match.
-      if (pcrs_string_cur == pcrs_string_next)
-      {
-        kmyth_log(LOG_ERR, "error parsing PCR string ... exiting");
-        return 1;
-      }
-
-      // Look at the first invalid character from the last call to strtol
-      // and confirm it's a blank, a comma, or '\0'. If not there's a disallowed
-      // character in the PCR string.
-      if (!isblank(*pcrs_string_next) && (*pcrs_string_next != ',')
-          && (*pcrs_string_next != '\0'))
-      {
-        kmyth_log(LOG_ERR, "invalid character (%c) in PCR string ... exiting",
-                  *pcrs_string_next);
-        return 1;
-      }
-
-      // Step past the invalid characters, checking not to skip past the
-      // end of the string.
-      while ((*pcrs_string_next != '\0')
-             && (isblank(*pcrs_string_next) || (*pcrs_string_next == ',')))
-      {
-        pcrs_string_next++;
-      }
-
-      // check that user entry specifies a valid PCR register. This is a more
-      // precise check the strtol overflow/underflow check used earlier.
-      if ((pcr_index_l < 0) || (pcr_index_l >= numPCRs))
-      {
-        kmyth_log(LOG_ERR,
-                  "TPM PCR %ld invalid, must be within range 0-%d ... exiting",
-                  pcr_index_l, numPCRs - 1);
-        return 1;
-      }
-
-      pcr_index = (int) pcr_index_l;
-      pcrs_struct->pcrSelections[0].pcrSelect[pcr_index / 8] |=
-        (1 << (pcr_index % 8));
-
-      pcrs_string_cur = pcrs_string_next;
-      pcrs_string_next = NULL;
+      kmyth_log(LOG_ERR, "failed to allocate memory for PCR list ... exiting");
+      return 1;
     }
+
+    if (parse_pcrs_string(pcrs_string, numPCRs, pcrs_list) != 0)
+    {
+      kmyth_log(LOG_ERR, "failed to parse PCR string ... exiting");
+      free(pcrs_list);
+      return 1;
+    }
+
+    for (int i = 0; i < numPCRs; i++)
+    {
+      if (pcrs_list[i])
+      {
+        pcrs_struct->pcrSelections[0].pcrSelect[i / 8] |= (1 << (i % 8));
+      }
+    }
+    free(pcrs_list);
 
     if (pcrs_struct->pcrSelections[0].sizeofSelect == 3)
     {
@@ -124,6 +86,85 @@ int init_pcr_selection(TSS2_SYS_CONTEXT * sapi_ctx, char *pcrs_string,
                 pcrs_struct->pcrSelections[0].pcrSelect[1],
                 pcrs_struct->pcrSelections[0].pcrSelect[0]);
     }
+  }
+
+  return 0;
+}
+
+//############################################################################
+// parse_pcrs_string()
+//############################################################################
+int parse_pcrs_string(char *pcrs_string, int num_pcrs, bool *pcrs_list)
+{
+  if (pcrs_string == NULL)
+  {
+    return 0;
+  }
+
+  if (pcrs_list == NULL)
+  {
+    return 1;
+  }
+  kmyth_log(LOG_DEBUG, "parsing PCR selection string");
+  memset(pcrs_list, 0, num_pcrs * sizeof(bool));
+
+  char *pcrs_string_cur = pcrs_string;
+  char *pcrs_string_next = NULL;
+
+  long pcr_index_l;
+
+  while (*pcrs_string_cur != '\0')
+  {
+    pcr_index_l = strtol(pcrs_string_cur, &pcrs_string_next, 10);
+
+    // Check for overflow or underflow on the strtol call. There
+    // really shouldn't be, because the number of PCRs is small.
+    if ((pcr_index_l == LONG_MIN) || (pcr_index_l == LONG_MAX))
+    {
+      kmyth_log(LOG_ERR, "invalid PCR value specified ... exiting");
+      return 1;
+    }
+
+    // Check that strtol didn't fail to parse an integer, which is the only
+    // condition that would cause the pointers to match.
+    if (pcrs_string_cur == pcrs_string_next)
+    {
+      kmyth_log(LOG_ERR, "error parsing PCR string ... exiting");
+      return 1;
+    }
+
+    // Look at the first invalid character from the last call to strtol
+    // and confirm it's a blank, a comma, or '\0'. If not there's a disallowed
+    // character in the PCR string.
+    if (!isblank(*pcrs_string_next) && (*pcrs_string_next != ',')
+        && (*pcrs_string_next != '\0'))
+    {
+      kmyth_log(LOG_ERR, "invalid character (%c) in PCR string ... exiting",
+                *pcrs_string_next);
+      return 1;
+    }
+
+    // Step past the invalid characters, checking not to skip past the
+    // end of the string.
+    while ((*pcrs_string_next != '\0')
+           && (isblank(*pcrs_string_next) || (*pcrs_string_next == ',')))
+    {
+      pcrs_string_next++;
+    }
+
+    // check that user entry specifies a valid PCR register. This is a more
+    // precise check the strtol overflow/underflow check used earlier.
+    if ((pcr_index_l < 0) || (pcr_index_l >= num_pcrs))
+    {
+      kmyth_log(LOG_ERR,
+                "TPM PCR %ld invalid, must be within range 0-%d ... exiting",
+                pcr_index_l, num_pcrs - 1);
+      return 1;
+    }
+    pcrs_list[(int) pcr_index_l] = 1;
+
+    pcrs_string_cur = pcrs_string_next;
+    pcrs_string_next = NULL;
   }
 
   return 0;
