5f069c029bdced447f552754a44618ef9de60ed9
Peter Hamilton <peter.allen.hamilton@gmail.com>
2021-06-10 10:24:05 -0400

    Add an implementation of Needham-Schroeder-Lowe for key retrieval
    This change adds a collection of functionality that implements the
    Needham-Schroeder-Lowe (NSL) protocol to support secure key
    retrieval without using TLS. NSL is used to create a shared session
    key between two parties which is then used to secure key requests
    and responses between the two parties.
    
    NSL utility functions can be found in nsl_util.h/c, socket utility
    functions can be found in socket_util.h/c, and KMIP utility
    functions can be found in kmip_util.h/c. Exemplars of the two
    communicating parties can be found in the nsl_client.c and
    nsl_server.c applications. Both support CLI arguments; use -h for
    more information when running them.

---
+++ tpm2/include/util/socket_util.h
@@ -0,0 +1,39 @@
+/**
+ * @file socket_util.h
+ *
+ * @brief Utility functions supporting raw socket operations.
+ */
+
+#ifndef SOCKET_UTIL_H
+#define SOCKET_UTIL_H
+
+/**
+ * <pre>
+ * This function sets up a client socket for sending messages.
+ * </pre>
+ *
+ * @param[in]  node       The IP address or hostname to connect to.
+ *
+ * @param[in]  service    The port number or service to bind to.
+ *
+ * @param[out] socket_fd  The new socket file descriptor.
+ *
+ * @return 0 on success, 1 on error
+ */
+int setup_client_socket(const char *node, const char *service,
+                        int *socket_fd);
+
+/**
+ * <pre>
+ * This function sets up a server socket for receiving connections.
+ * </pre>
+ *
+ * @param[in]  service    The port number to bind to.
+ *
+ * @param[out] socket_fd  The new socket file descriptor.
+ *
+ * @return 0 on success, 1 on error
+ */
+int setup_server_socket(const char *service, int *socket_fd);
+
+#endif
---
+++ tpm2/src/main/nsl_server.c
@@ -0,0 +1,167 @@
+#include <getopt.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+
+#include <kmip/kmip.h>
+
+#include "defines.h"
+#include "nsl_util.h"
+#include "socket_util.h"
+#include "kmip_util.h"
+#include "aes_gcm.h"
+
+static void usage(const char *prog)
+{
+  fprintf(stdout,
+          "\nusage: %s [options]\n\n"
+          "options are :\n\n"
+          "Server Information --\n"
+          "  -k or --key   Path to the file containing the server's private key.\n"
+          "  -p or --port  The port number to connect to.\n"
+          "Client Information --\n"
+          "  -c or --cert  Path to the file containing the client's public key.\n"
+          "Misc --\n" "  -h or --help  Help (displays this usage).\n\n", prog);
+}
+
+int check_string_arg(const char *arg, size_t arg_len,
+                     const char *value, size_t value_len)
+{
+  if ((arg_len != value_len) || strncmp(arg, value, value_len))
+  {
+    return 0;
+  }
+  return 1;
+}
+
+const struct option longopts[] = {
+  // Server info
+  {"key", required_argument, 0, 'k'},
+  {"port", required_argument, 0, 'p'},
+  // Client info
+  {"cert", required_argument, 0, 'c'},
+  // Misc
+  {"help", no_argument, 0, 'h'},
+  {0, 0, 0, 0}
+};
+
+int main(int argc, char **argv)
+{
+  // Exit early if there are no arguments.
+  if (1 == argc)
+  {
+    usage(argv[0]);
+    return 0;
+  }
+
+  char *key = NULL;
+  char *port = NULL;
+  char *cert = NULL;
+
+  int options;
+  int option_index;
+
+  while ((options =
+          getopt_long(argc, argv, "k:p:c:h", longopts, &option_index)) != -1)
+  {
+    switch (options)
+    {
+      // Server info
+    case 'k':
+      key = optarg;
+      break;
+    case 'p':
+      port = optarg;
+      break;
+      // Client info
+    case 'c':
+      cert = optarg;
+      break;
+      // Misc
+    case 'h':
+      usage(argv[0]);
+      return 0;
+    default:
+      return 1;
+    }
+  }
+
+  //set_applog_severity_threshold(7);
+
+  // Create server socket
+  kmyth_log(LOG_INFO, "Setting up server socket");
+
+  int socket_fd = -1;
+  int result = setup_server_socket(port, &socket_fd);
+
+  if (result)
+  {
+    kmyth_log(LOG_ERR, "Failed to setup server socket.");
+    return 1;
+  }
+
+  // Load public/private keys; create EVP contexts
+  EVP_PKEY_CTX *public_key_ctx = setup_public_evp_context(cert);
+
+  if (NULL == public_key_ctx)
+  {
+    kmyth_log(LOG_ERR, "Failed to setup public EVP context.");
+    close(socket_fd);
+    return 1;
+  }
+  EVP_PKEY_CTX *private_key_ctx = setup_private_evp_context(key);
+
+  if (NULL == private_key_ctx)
+  {
+    kmyth_log(LOG_ERR, "Failed to setup the private EVP context.");
+    EVP_PKEY_CTX_free(public_key_ctx);
+    close(socket_fd);
+    return 1;
+  }
+
+  // Conduct NSL to obtain a shared session key
+  unsigned char *id = (unsigned char *) "B\0";
+  size_t id_len = 2;
+
+  unsigned char *session_key = NULL;
+  size_t session_key_len = 0;
+
+  result = negotiate_server_session_key(socket_fd,
+                                        public_key_ctx,
+                                        private_key_ctx,
+                                        id, id_len,
+                                        &session_key, &session_key_len);
+
+  if (result)
+  {
+    kmyth_log(LOG_ERR, "Failed to negotiate the server session key.");
+    close(socket_fd);
+    return 1;
+  }
+
+  EVP_PKEY_CTX_free(public_key_ctx);
+  EVP_PKEY_CTX_free(private_key_ctx);
+
+  // Send key K to A; encrypt message with S
+  uint8 static_key[16] = {
+    0xD3, 0x51, 0x91, 0x0F, 0x1D, 0x79, 0x34, 0xD6,
+    0xE2, 0xAE, 0x17, 0x57, 0x65, 0x64, 0xE2, 0xBC
+  };
+  kmyth_log(LOG_INFO, "Loaded symmetric key: 0x%02X..%02X", static_key[0],
+            static_key[15]);
+
+  result = send_key_with_session_key(socket_fd,
+                                     session_key, session_key_len,
+                                     static_key, 16);
+  if (result)
+  {
+    kmyth_log(LOG_ERR, "Failed to send the static key.");
+    close(socket_fd);
+    return 1;
+  }
+
+  close(socket_fd);
+
+  return 0;
+}
