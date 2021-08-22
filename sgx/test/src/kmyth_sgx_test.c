6915d5761d63dfa036bd49188fd6c4962372f6c1
b-carr <53411033+b-carr@users.noreply.github.com>
2021-08-22 08:18:22 -0400

    Added initial test for unseal-into-enclave and a degugging helper function.

--- sgx/test/src/kmyth_sgx_test.c
+++ sgx/test/src/kmyth_sgx_test.c
@@ -8,10 +8,22 @@
 #include "sgx_urts.h"
 #include "sgx_attributes.h"
 #include "kmyth_sgx_test_enclave_u.h"
+//#include "kmyth_enclave.h"
 
 // NB: Should specify as an absolute path.
 #define ENCLAVE_PATH "sgx/kmyth_sgx_test_enclave.signed.so"
 sgx_enclave_id_t eid = 0;
+
+void ocall_print_table_entry(size_t size, uint8_t * data)
+{
+  printf("%lu\n", size);
+  for (size_t i = 0; i < size; i++)
+  {
+    printf("0x%02x ", data[i]);
+  }
+  printf("\n");
+  return;
+}
 
 int init_suite(void)
 {
@@ -52,6 +64,10 @@ void test_enclave_seal_unseal(void)
   CU_ASSERT(sgx_ret == 0);
 
   in_data = (uint8_t *) calloc(in_size, 1);
+  for (size_t i = 0; i < in_size; i++)
+  {
+    in_data[i] = (uint8_t) i;
+  }
   out_data_decrypted = (uint8_t *) malloc(in_size);
   out_data = (uint8_t *) malloc(out_size);
 
@@ -68,10 +84,47 @@ void test_enclave_seal_unseal(void)
                   in_size);
   CU_ASSERT(sgx_ret == 0);
   CU_ASSERT(memcmp(in_data, out_data_decrypted, in_size) == 0);
-
   free(in_data);
   free(out_data_decrypted);
   free(out_data);
+  return;
+}
+
+void test_unseal_and_export(void)
+{
+  uint8_t *in_data = NULL;
+  uint8_t *out_data = NULL;
+
+  uint8_t *out_data_decrypted = NULL;
+
+  int sgx_ret;
+  size_t in_size = 8;
+  size_t out_size = 0;
+
+  uint16_t key_policy = SGX_KEYPOLICY_MRSIGNER;
+  sgx_attributes_t attribute_mask;
+
+  attribute_mask.flags = 0;
+  attribute_mask.xfrm = 0;
+
+  enc_get_sealed_size(eid, &sgx_ret, in_size, (uint32_t *) & out_size);
+  CU_ASSERT(sgx_ret == 0);
+
+  in_data = (uint8_t *) calloc(in_size, 1);
+  out_data_decrypted = (uint8_t *) malloc(in_size);
+  out_data = (uint8_t *) malloc(out_size);
+
+  enc_seal_data(eid, &sgx_ret, in_data, in_size, out_data, out_size, key_policy,
+                attribute_mask);
+
+  kmyth_unsealed_data_table_initialize(eid, &sgx_ret);
+  CU_ASSERT(sgx_ret == 0);
+
+  kmyth_unseal_into_enclave(eid, &sgx_ret, out_size, out_data);
+  CU_ASSERT(sgx_ret == 0);
+
+  //  kmyth_unsealed_data_table_cleanup(eid, &sgx_ret);
+  //CU_ASSERT(sgx_ret == 0);
   return;
 }
 
@@ -95,6 +148,12 @@ int main(void)
   if (NULL ==
       CU_add_test(kmyth_sgx_test_suite, "Test enclave seal/unseal",
                   test_enclave_seal_unseal))
+  {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+  if (NULL == CU_add_test(kmyth_sgx_test_suite, "Test enclave unseal table",
+                          test_unseal_and_export))
   {
     CU_cleanup_registry();
     return CU_get_error();
