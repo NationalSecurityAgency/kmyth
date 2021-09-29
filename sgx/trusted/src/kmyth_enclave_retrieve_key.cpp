1c17e727c161b058fd27f6cffea8022419e58fb5
bit-bender <53864796+bit-bender@users.noreply.github.com>
2021-09-29 14:25:22 -0400

    added clear and free of client key and server cert in enclave

--- sgx/trusted/src/kmyth_enclave_retrieve_key.cpp
+++ sgx/trusted/src/kmyth_enclave_retrieve_key.cpp
@@ -54,5 +54,8 @@ int kmyth_enclave_retrieve_key_from_server(uint8_t * client_private_bytes,
   kmyth_enclave_clear(server_cert_bytes, server_cert_bytes_len);
   enclave_log(7, "cleared memory for DER formatted server certificate");
 
+  kmyth_enclave_clear_and_free(client_sign_key, sizeof(client_sign_key));
+  kmyth_enclave_clear_and_free(server_cert, sizeof(server_cert));
+
   return EXIT_SUCCESS;
 }
