54a22bb3d9347eedc3416e3e19cca8f176906890
bit-bender <53864796+bit-bender@users.noreply.github.com>
2022-10-25 15:14:02 -0400

    removed unused allocate_byte_buffer() utility

--- utils/src/memory_util.c
+++ utils/src/memory_util.c
@@ -47,29 +47,3 @@ void *secure_memset(void *v, int c, size_t n)
 
   return v;
 }
-
-//############################################################################
-// allocate_buffer()
-//############################################################################
-uint8_t *allocate_byte_buffer(uint8_t *v, size_t n)
-{
-  uint8_t *temp = NULL;
-
-  if (v == NULL)
-  {
-    temp = calloc(n, 1);
-  }
-  else
-  {
-    //temp = realloc(v, n);
-    //kmyth_clear(temp, n);
-  }
-
-  //if (sizeof(temp) != n)
-  //{
-  //  free(temp);
-  //  temp = NULL;
-  //}
-
-  return temp;
-}
