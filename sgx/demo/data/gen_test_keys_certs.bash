4dd2cf9ef0fe6228321eb6bb62f2ac6890a50cd4
bit-bender <53864796+bit-bender@users.noreply.github.com>
2023-11-07 12:22:05 -0500

    updated ECDH demo key/cert creation script

--- sgx/demo/data/gen_test_keys_certs.bash
+++ sgx/demo/data/gen_test_keys_certs.bash
@@ -1,14 +1,75 @@
-openssl ecparam -name secp521r1 -genkey -noout -out ca_priv_test.pem
-openssl req -new -x509 -key ca_priv_test.pem -subj "/C=US/O=Kmyth/CN=TestCA" -out ca_cert_test.pem -days 365
+# create key and certificate for test "Certificate Authority (CA)"
+openssl ecparam -name secp521r1 \
+                -genkey \
+                -noout \
+                -out ca_priv_test.pem
+openssl req -new \
+            -x509 \
+            -key ca_priv_test.pem \
+            -subj "/C=US/O=Kmyth/CN=TestCA" \
+            -sha256 \
+            -days 365 \
+            -out ca_cert_test.pem
 
-openssl ecparam -name secp521r1 -genkey -noout -out client_priv_test.pem
-openssl req -new -sha256 -key client_priv_test.pem -subj "/C=US/O=Kmyth/CN=TestClient" -out client_cert_test.csr
-openssl x509 -req -in client_cert_test.csr -CA ca_cert_test.pem -CAkey ca_priv_test.pem -CAcreateserial -out client_cert_test.pem -days 365 -sha256
+# create key and certificate for test "client"
+openssl ecparam -name secp521r1 \
+                -genkey \
+                -noout \
+                -out client_priv_test.pem
+openssl req -new \
+            -key client_priv_test.pem \
+            -subj "/C=US/O=Kmyth/CN=TestClient" \
+            -addext "subjectAltName = IP:127.0.0.1" \
+            -sha256 \
+            -out client_cert_test.csr
+openssl x509 -req \
+             -in client_cert_test.csr \
+             -CA ca_cert_test.pem \
+             -CAkey ca_priv_test.pem \
+             -copy_extensions copy \
+             -CAcreateserial \
+             -sha256 \
+             -days 365 \
+             -out client_cert_test.pem
 
-openssl ecparam -name secp521r1 -genkey -noout -out proxy_priv_test.pem
-openssl req -new -sha256 -key proxy_priv_test.pem -subj "/C=US/O=Kmyth/CN=TestProxy" -out proxy_cert_test.csr
-openssl x509 -req -in proxy_cert_test.csr -CA ca_cert_test.pem -CAkey ca_priv_test.pem -CAcreateserial -out proxy_cert_test.pem -days 365 -sha256
+# create key and certificate for test "ECDH proxy"
+openssl ecparam -name secp521r1 \
+                -genkey \
+                -noout \
+                -out proxy_priv_test.pem
+openssl req -new \
+            -key proxy_priv_test.pem \
+            -subj "/C=US/O=Kmyth/CN=TestProxy" \
+            -addext "subjectAltName = IP:127.0.0.1" \
+            -sha256 \
+            -out proxy_cert_test.csr
+openssl x509 -req \
+             -in proxy_cert_test.csr \
+             -CA ca_cert_test.pem \
+             -CAkey ca_priv_test.pem \
+             -copy_extensions copy \
+             -CAcreateserial \
+             -sha256 \
+             -days 365 \
+             -out proxy_cert_test.pem
 
-openssl ecparam -name secp521r1 -genkey -noout -out server_priv_test.pem
-openssl req -new -sha256 -key server_priv_test.pem -subj "/C=US/O=Kmyth/CN=127.0.0.1" -out server_cert_test.csr
-openssl x509 -req -in server_cert_test.csr -CA ca_cert_test.pem -CAkey ca_priv_test.pem -CAcreateserial -out server_cert_test.pem -days 365 -sha256
+# create key and certificate for test "key server"
+openssl ecparam -name secp521r1 \
+                -genkey \
+                -noout \
+                -out server_priv_test.pem
+openssl req -new \
+            -key server_priv_test.pem \
+            -subj "/C=US/O=Kmyth/CN=TestServer" \
+            -addext "subjectAltName = IP:127.0.0.1" \
+            -sha256 \
+            -out server_cert_test.csr
+openssl x509 -req \
+             -in server_cert_test.csr \
+             -CA ca_cert_test.pem \
+             -CAkey ca_priv_test.pem \
+             -copy_extensions copy \
+             -CAcreateserial \
+             -sha256 \
+             -days 365 \
+             -out server_cert_test.pem
