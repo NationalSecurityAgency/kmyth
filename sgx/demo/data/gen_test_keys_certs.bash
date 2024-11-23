d3ecf104c7f676562b859a60e1b546d7189854a8
bit-bender <53864796+bit-bender@users.noreply.github.com>
2024-11-23 10:44:58 -0500

    Add -fPIE option to SGX C++ file compilation / Address TLS issue with SGX ECDH demonstration (#197)
    * refactored test key/cert gneration for SGX ECDH demo
    
    * added 'valgrind-devel' package as a dependency in INSTALL.md

--- sgx/demo/data/gen_test_keys_certs.bash
+++ sgx/demo/data/gen_test_keys_certs.bash
@@ -1,75 +1,53 @@
 # create key and certificate for test "Certificate Authority (CA)"
-openssl ecparam -name secp521r1 \
-                -genkey \
-                -noout \
-                -out ca_priv.pem
-
-openssl req -new \
-            -x509 \
-            -config ca.cnf \
-            -key ca_priv.pem \
-            -days 365 \
+openssl req -x509 \
+            -batch \
+            -noenc \
+            -newkey ec \
+            -pkeyopt ec_paramgen_curve:secp521r1 \
+            -config openssl.cnf \
+            -section req_ca \
+            -days 30 \
+            -keyout ca_priv.pem \
             -out ca_cert.pem
 
 # create key and certificate for test "client"
-openssl ecparam -name secp521r1 \
-                -genkey \
-                -noout \
-                -out client_priv.pem
-
-openssl req -new \
-            -config client.cnf \
-            -key client_priv.pem \
-            -out client.csr
-
-openssl x509 -req \
-             -in client.csr \
-             -extfile client.cnf \
-             -extensions v3_ext \
-             -CA ca_cert.pem \
-             -CAkey ca_priv.pem \
-             -CAcreateserial \
-             -days 365 \
-             -out client_cert.pem
+openssl req -x509 \
+            -batch \
+            -noenc \
+            -newkey ec \
+            -pkeyopt ec_paramgen_curve:secp521r1 \
+            -config openssl.cnf \
+            -section req_client \
+            -CA ca_cert.pem \
+            -CAkey ca_priv.pem \
+            -days 30 \
+            -keyout client_priv.pem \
+            -out client_cert.pem
 
 # create key and certificate for test "ECDH proxy"
-openssl ecparam -name secp521r1 \
-                -genkey \
-                -noout \
-                -out proxy_priv.pem
-
-openssl req -new \
-            -config proxy.cnf \
-            -key proxy_priv.pem \
-            -out proxy.csr
-
-openssl x509 -req \
-             -in proxy.csr \
-             -extfile proxy.cnf \
-             -extensions v3_ext \
-             -CA ca_cert.pem \
-             -CAkey ca_priv.pem \
-             -CAcreateserial \
-             -days 365 \
-             -out proxy_cert.pem
+openssl req -x509 \
+            -batch \
+            -noenc \
+            -newkey ec \
+            -pkeyopt ec_paramgen_curve:secp521r1 \
+            -config openssl.cnf \
+            -section req_proxy \
+            -CA ca_cert.pem \
+            -CAkey ca_priv.pem \
+            -days 30 \
+            -keyout proxy_priv.pem \
+            -out proxy_cert.pem
 
 # create key and certificate for test "key server"
-openssl ecparam -name secp521r1 \
-                -genkey \
-                -noout \
-                -out server_priv.pem
-
-openssl req -new \
-            -config server.cnf \
-            -key server_priv.pem \
-            -out server.csr
-            
-openssl x509 -req \
-             -in server.csr \
-             -extfile server.cnf \
-             -extensions v3_ext \
-             -CA ca_cert.pem \
-             -CAkey ca_priv.pem \
-             -CAcreateserial \
-             -days 365 \
-             -out server_cert.pem
+openssl req -x509 \
+            -batch \
+            -noenc \
+            -newkey ec \
+            -pkeyopt ec_paramgen_curve:secp521r1 \
+            -config openssl.cnf \
+            -section req_server \
+            -CA ca_cert.pem \
+            -CAkey ca_priv.pem \
+            -days 30 \
+            -keyout server_priv.pem \
+            -out server_cert.pem
